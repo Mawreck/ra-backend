c5cd598c8d5c8ac3ca5fca04e7ef3256bfb159e7
Mawreck <marek.ounpuu@srini.ee>
2023-03-09 03:44:37 +0200

    feat(resources): add data initialization

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO users (id, username, full_name)
+VALUES ('1', 'Bret', 'Leanne Graham'),
+       ('2', 'Antonette', 'Ervin Howell'),
+       ('3', 'Samantha', 'Clementine Bauch'),
+       ('4', 'Karianne', 'Patricia Lebsack'),
+       ('5', 'Kamren', 'Chelsey Dietrich'),
+       ('6', 'Leopoldo_Corkery', 'Mrs. Dennis Schulist'),
+       ('7', 'Elwyn.Skiles', 'Kurtis Weissnat'),
+       ('8', 'Maxime_Nienow', 'Nicholas Runolfsdottir V'),
+       ('9', 'Delphine', 'Glenna Reichert'),
+       ('10', 'Moriah.Stanton', 'Moriah Stanton');
